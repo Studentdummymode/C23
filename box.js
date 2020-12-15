edf60732459a6782831092ffbe9cf04c7f0badc4
Studentdummymode <73488378+Studentdummymode@users.noreply.github.com>
2020-12-15 18:47:41 -0500

    Add files via upload

---
+++ box.js
@@ -0,0 +1,19 @@
+class box{
+    constructor(x,y,width,height){
+        var options={
+            resitution: 1
+        }
+        this.body = Bodies.rectangle(x,y,width,height),options;
+        this.width = width;
+        this.height = height;
+        World.add(world,this.body);
+    }
+
+    display(){
+        var pos = this.body.position;
+        rectMode(CENTER);
+        fill("pink");
+        rect(pos.x,pos.y,this.width,this.height)
+
+    }
+}
